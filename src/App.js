b0974766ab0cde4b73ac011904322067de150ee8
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-31 18:27:55 +0100

    buttons component starter file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Button from "./components/Button";
+
 function App() {
-  return <div>My App</div>
+  return (
+    <div>
+      <div>
+        <Button>Button 1</Button>
+      </div>
+      <div>
+        <Button>Button 2</Button>
+      </div>
+      <div>
+        <Button>Button 3</Button>
+      </div>
+      <div>
+        <Button>Button 4</Button>
+      </div>
+      <div>
+        <Button>Button 5</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
+  return <button>{children}</button>;
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of the following can be true: primary, secondary, success, warning or danger."
+      );
+    }
+  },
+};
+
+export default Button;
