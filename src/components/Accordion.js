22ffd2feb87e1b9d5e118159deecd0b87fff198d
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-06-01 15:40:23 +0100

    hiding content implemented

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,24 @@
-function Accordion() {
-  return <div>Accordion</div>;
+import { useState } from "react";
+
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    return (
+      <div key={item.id}>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
