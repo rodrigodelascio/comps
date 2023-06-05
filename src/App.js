6dc39f083a19d95542283fc280343442d352748f
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-06-05 15:22:17 +0100

    navigation fully implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
