7dcb84c14aa314a79b794bb91c4b758f409e9cbb
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-06-06 14:38:21 +0100

    table dynamic functionality implemented

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -2,15 +2,28 @@ import Table from "../components/Table";
 
 function TablePage() {
   const data = [
-    { name: "Orange", color: "bg-orange-400", score: 2 },
-    { name: "Apple", color: "bg-red-500", score: 3 },
-    { name: "Banana", color: "bg-yellow-300", score: 1 },
-    { name: "Lime", color: "bg-green-400", score: 5 },
+    { name: "Orange", colour: "bg-orange-400", score: 2 },
+    { name: "Apple", colour: "bg-red-500", score: 3 },
+    { name: "Banana", colour: "bg-yellow-300", score: 1 },
+    { name: "Lime", colour: "bg-green-400", score: 5 },
   ];
+
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Colour",
+      render: (fruit) => <div className={`p-3  m-2 ${fruit.colour}`}></div>,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
 
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
