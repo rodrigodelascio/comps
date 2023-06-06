b485db0d3c0645f6f56ae1104f5b538c8eefea79
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-06-06 18:03:33 +0100

    initial sorting for tables

---
+++ src/components/SortableTable.js
@@ -0,0 +1,24 @@
+import Table from "./Table";
+
+function SortableTable(props) {
+  const { config } = props;
+
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+
+    return {
+      ...column,
+      header: () => <th>{column.label}</th>,
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
+}
+
+export default SortableTable;
